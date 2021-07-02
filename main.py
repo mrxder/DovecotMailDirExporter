c79909840b5218c45e268fee2f2c9618d3b7278b
Matthias Keim <matthias@keim.dev>
2021-07-02 19:09:53 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import datetime
+
+def editMail(basePaht : str, filename :str):
+
+    fullPath = os.path.join(basePaht, filename)
+
+    rawDate = ""
+    rawSubject = ""
+    with open(fullPath, 'r') as file_in:
+        for line in file_in:
+
+            if len(line) > 6 and line[0 : 5] == "Date:":
+                rawDate = line
+            
+            if "Subject:" in line:
+                rawSubject = line
+            
+            if len(rawDate) > 1 and len(rawSubject) > 1:
+                break
+
+    rawDate = rawDate[6:-1]
+    partsDate = rawDate.split(',')
+    rawDate = partsDate[1]
+    partsDate = rawDate.split(' ')
+    rawDate = ""
+    for pdel in partsDate:
+        if len(pdel) > 0 and (pdel[0] == "+" or pdel[0] == '-'):
+            break
+        if len(pdel) > 0:
+            rawDate += pdel + "_"
+    os.rename(fullPath, os.path.join(basePaht, rawDate + ".eml"))
+
+def travers(dir : str):
+    elems = os.listdir(dir)
+    for elem in elems:
+        if elem[0] != '.':
+            
+            elemPath = os.path.join(dir, elem)
+            if os.path.isfile(elemPath):
+                if ".mail" in elem and "W=" in elem:
+                    editMail(dir, elem)
+                else:
+                    os.remove(elemPath)
+            
+            if os.path.isdir(elemPath):
+                travers(elemPath)
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Specify directory")
+    else:
+        travers(sys.argv[1])
